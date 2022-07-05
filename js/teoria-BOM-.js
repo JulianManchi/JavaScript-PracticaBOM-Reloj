31b541f96ede9d518f69483deaaac8a4b682d649
Julian Mancinelli <Julianomanchi03@gmail.com>
2022-07-05 18:02:11 -0300

    Agregamos setInterval

--- js/teoria-BOM-.js
+++ js/teoria-BOM-.js
@@ -1,19 +1,32 @@
+let contador = 1;
+
 // Ejecutar una funcion pasado cierto tiempo en milisegundos
 
 // Primer metodo (No se usa muy habitualmente)
 function mostarPublicidad(){
+    if(contador === 5){
+        // quiero obtener el intervalo
+        window.clearInterval(codigo);
+    }
     document.write(`<p>Ahora muestro publicidad</p>`);
+    contador = contador + 1;
 }
 
-window.setTimeout(mostarPublicidad, 3000);
+// window.setTimeout(mostarPublicidad, 3000);
 
 // Segundo metodo (Se usa habitualmente)
-window.setTimeout(function (){
-    document.write(`<p>Ahora muestro publicidad</p>`);
-}, 4000);
+// window.setTimeout(function (){
+//     document.write(`<p>Ahora muestro publicidad</p>`);
+// }, 4000);
 
-// Tercer metodo (Se usa habitualmente)
-setTimeout(()=>{
-    document.write(`<p>Ahora muestro publicidad</p>`);
-}, 4500);
+// // Tercer metodo (Se usa habitualmente)
+// setTimeout(()=>{
+//     document.write(`<p>Ahora muestro publicidad</p>`);
+// }, 4500);
 
+// Ejecutar una funcion cada cierto tiempo en milisegundo y que se repita
+// setInterval(() => {
+//     document.write(`<p>Ahora muestro publicidad</p>`);
+// }, 5000);
+
+let codigo = setInterval(mostarPublicidad, 3000);
